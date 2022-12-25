733d3187ffd60cf56c9088108b3170e481190232
Edward <edwardsoen@gmail.com>
2022-12-26 00:39:42 +0800

    made some changes

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+//#region Objects
+
 function Problem() { 
     this.name
     this.isPremium
@@ -10,6 +12,84 @@ function Company() {
     this.button
 }
 
+function PageData() { 
+    this.problemData = []
+    this.companiesData = []
+}
+
+
+//#endregion
+
+function grabData() { 
+    let url = "https://www.npoint.io/documents/b37ca078aab84f3e1a80"
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); 
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText)["contents"]
+}
+
+
+
+function getProblemSets() {     
+    let problemsets = document.querySelectorAll('[role="row"]')
+    let problems = []
+
+    for(let i =1; i <= problemsets.length -1 ; i ++) { 
+        let cells = problemsets[i].querySelectorAll('[role="cell"]')
+        let href = problemsets[i].getElementsByTagName('a')[0].href
+
+        let problemName = getProblem(href)
+        let slider = cells[cells.length-1]
+
+        let isPremiumProblem = cells[1].getElementsByTagName('svg').length >= 1
+
+        let problemOject = new Problem(); 
+        problemOject.name = problemName; 
+        problemOject.isPremium = isPremiumProblem; 
+        problemOject.slider = slider; 
+
+        problems.push(problemOject)
+    
+    }
+    return problems
+}
+
+
+function getProblem(url) {
+    data = url.split("/") 
+    return data[data.length-2]
+}
+
+function getActiveCompaniesTags() { 
+    data = []
+    let swiper = document.getElementsByClassName('swiper-slide-active')[2]
+    let links = swiper.getElementsByTagName('a')
+    for(let ii = 0; ii <= links.length-1; ii ++) {
+        let link = links[ii].href.split("/") 
+        links[ii].href = "#"
+        let companyName = links[ii].firstChild.firstChild.textContent
+        let companyObject = new Company()
+        companyObject.name = companyName; 
+        companyObject.button = links[ii]
+        data.push(companyObject)
+    }
+    return data
+}
+
+
+function addObserverToCompaniesSection(data) {
+    var composeBox =  document.getElementsByClassName('mt-0')[0]
+    const observer = new MutationObserver(() => {
+        addClickListenerToCompanyButton(getActiveCompaniesTags(), data)
+    });
+
+    if(!composeBox) {
+        window.setTimeout(() => {addObserverToCompaniesSection(data)} ,500);
+        return;
+    }
+    var config = {childList: true, subtree: true, attributes: true};
+    observer.observe(composeBox,config);
+}
 
 function PageAnalyzer () { 
     this.getPageInfo = function () { 
@@ -18,144 +98,114 @@ function PageAnalyzer () {
 
         if(expression.includes("problems")) { 
             let problem = getProblem(this.url)
+            let pageData = new PageData(); 
+            pageData.problemData = data
+            return pageData
         }
 
         if (expression.includes("problemset")) { 
             let data = getProblemSets()
-            let CompanyData = getCompaniesTags()
+            let CompanyData = getActiveCompaniesTags()
+            let pageData = new PageData(); 
+            pageData.problemData = data
+            pageData.companiesData = CompanyData
+            return pageData
         }
     }
 
 
-    function getProblemSets() {     
-        let problemsets = document.querySelectorAll('[role="row"]')
-        let problems = []
-
-        for(let i =1; i <= problemsets.length -1 ; i ++) { 
-            let cells = problemsets[i].querySelectorAll('[role="cell"]')
-            let href = problemsets[i].getElementsByTagName('a')[0].href
-
-            let problemName = getProblem(href)
-            let slider = cells[cells.length-1]
-
-            let isPremiumProblem = cells[1].getElementsByTagName('svg').length >= 1
-
-            let problemOject = new Problem(); 
-            problemOject.name = problemName; 
-            problemOject.isPremium = isPremiumProblem; 
-            problemOject.slider = slider; 
-
-            problems.push(problemOject)
-        
-        }
-        return problems
-    }
-
-
-    function getProblem(url) {
-        data = url.split("/") 
-        console.log(data[data.length-2])
-        return data[data.length-2]
-    }
-
-    function getCompaniesTags() { 
-        data = []
-        let swiper = document.getElementsByClassName('swiper-slide')
-        for(let i = 0; i <= swiper.length -1; i ++) {
-            let links = swiper[i].getElementsByTagName('a')
-            for(let ii = 0; ii <= links.length-1; ii ++) {
-                let link = links[ii].href.split("/") 
-                links[ii].href = "#"
-                let companyName = link[link.length-1]
-                let companyObject = new Company()
-                companyObject.name = companyName; 
-                companyObject.button = links[ii]
-                
-                data.push(companyName)
-            }
-        } 
-        return data
-    }
 }
 
 
 
 
-
-
-
-function setSliderData() { 
-
-}
+//#region on Company button clicked
 
 function setCompanyData(data) {
     let parentDiv = document.createElement('div')
-    
-    
-    
-    
-    let row = document.createElement('div')
-    row.style = `
-    display:flex;
-    justify-content:space-around; 
-    border-bottom: solid 1px black;
-    border-top: solid 1px black;
-    `
-    let occuranceCell = createCell("122")
-    let difficultyCell = createCell("Hard")
 
-    let problemCell = document.createElement('div')
-    let a = document.createElement('a')
-    a.href = "testing"
-    a.textContent = "hello"
-    a.style = `
-    display: block;
-    font-size: 1.5em;
-    margin-block-start: 0.83em;
-    margin-block-end: 0.83em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    font-weight: bold;
-    `
-    problemCell.appendChild(a)
-    
+    for(let i = 0; i <= data.length-1; i ++) {        
+        let row = document.createElement('div')
+        row.style = `
+        display:flex;
+        justify-content:space-around; 
+        border-bottom: solid 1px black;
+        border-top: solid 1px black;
+        `
 
-
-
-    row.appendChild(occuranceCell)
-    row.appendChild(problemCell)
-    row.appendChild(difficultyCell)
-
-
-
-    
-
-
-    parentDiv.append(row)
+        
+        let occuranceCell = createCell(data[i]["occurance"])
+        let difficultyCell = createCell("Hard")
+        let problemCell = document.createElement('div')
+        let a = document.createElement('a')
+        a.href = data[i]["url"]
+        a.textContent = data[i]["problem"]
+        a.style = `
+        display: block;
+        font-size: 1.17em;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        font-weight: bold;
+        color: inherit; 
+        `
+        problemCell.appendChild(a)
+        row.appendChild(occuranceCell)
+        row.appendChild(problemCell)
+        row.appendChild(difficultyCell)
+        parentDiv.append(row)
+    }
     return parentDiv
 }
 
 
 function createCell(text) { 
     let div = document.createElement('div')
-    let h2 = document.createElement('h2')
-    h2.textContent = text
-    div.appendChild(h2)
+    let h3 = document.createElement('h3')
+    h3.textContent = text
+    div.appendChild(h3)
     return div
 }
 
 
-new PageAnalyzer().getPageInfo()
-
-
-
-
-
-
-
 function openPopupWindow(divContent) {
-    popupWindow = window.open("",'_blank','height=500,width=500,left=100,top=100');
+    popupWindow = window.open("",'_blank','height=500,width=1000,left=100,top=100');
     popupWindow.document.body.appendChild(divContent); 
 }
 
-openPopupWindow(setCompanyData())
+function createOnClickFunction(data) { 
+    return () => {
+        openPopupWindow(setCompanyData(data))
+    }
+}
+
+//#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+function addClickListenerToCompanyButton(companyList, premiumData) { 
+    for(let i =0; i <= companyList.length -1; i ++) { 
+        let companyName = companyList[i].name 
+        companyList[i].button.addEventListener("click", createOnClickFunction(premiumData[companyName]));
+    }
+}
+
+
+
+
+
+
+
+var data = grabData()
+// new PageAnalyzer().getPageInfo()
+addObserverToCompaniesSection(data)
